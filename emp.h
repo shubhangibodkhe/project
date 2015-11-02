b2d802ebc051df1b4e1857b372e683d35d3f1f29
shubhangibodkhe <bodkhesn15.it@coep.ac.in>
2015-11-02 13:49:40 +0530

    Update emp.h

--- emp.h
+++ emp.h
@@ -5,19 +5,22 @@
 
 struct emp{
 	int emp_id;
-        char name[40]; 
+        char name[50]; 
         int age; 
 	char address[50];
         float salary;
 	int year_of_experience;
-	//int DOB;
-	char blood_group[10];
+	char post[50];
+	
 };
 
 struct emp e;
 
+void mainmenu();
 void add();
+int already(int);
 void display();
 void delete();
 void modify();
+void search();
 
